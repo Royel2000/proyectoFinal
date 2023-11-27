7f39c7de4166600dda812ea9a4bb7af144699945
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2023-11-26 22:12:29 -0500

    style: Alumno, perf:rutas
    Se hizo unos cambios en la apariencia de la tabla alumnos y aplicando las rutas a la tabla alumnos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './pages/login/login.component';
+import { AlumnoComponent } from './components/alumno/alumno.component';
 
 
 const routes: Routes = [
+  { path: 'alumno', component: AlumnoComponent },
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
 ];
