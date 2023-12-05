8b7299e3f1c18e29793b6661fced33e4450e218b
AyalaManu <114090237+AyalaManu@users.noreply.github.com>
2023-12-05 05:26:11 -0500

    perf: actualizar

--- src/app/components/tutorial-details/tutorial-details.component.ts
+++ src/app/components/tutorial-details/tutorial-details.component.ts
@@ -8,6 +8,12 @@ import { TutorialService } from 'src/app/services/tutorial.service';
   styleUrls: ['./tutorial-details.component.css']
 })
 export class TutorialDetailsComponent implements OnInit {
+  calificacion1: number | null = null;
+  calificacion2: number | null = null;
+  calificacion3: number | null = null;
+  promedio: number | null = null;
+  submitted: boolean = false;
+
   @Input() tutorial?: Rol;
   @Output() refreshList: EventEmitter<any> = new EventEmitter();
   currentTutorial: Rol = {
@@ -49,7 +55,9 @@ export class TutorialDetailsComponent implements OnInit {
     const data = {
       nombre: this.currentTutorial.nombre,
       apellido: this.currentTutorial.apellido,
-      grupo: this.currentTutorial.grupo
+      grupo: this.currentTutorial.grupo,
+      carrera: this.currentTutorial.carrera,
+      sexo: this.currentTutorial.sexo
     };
 
     if (this.currentTutorial.id) {
@@ -69,4 +77,38 @@ export class TutorialDetailsComponent implements OnInit {
         .catch(err => console.log(err));
     }
   }
+
+  calcularPromedio() {
+    if (!this.sonCalificacionesValidas()) {
+      alert('Ingresa calificaciones válidas entre 0 y 100.');
+      return;
+    }
+
+    this.promedio = (this.calificacion1! + this.calificacion2! + this.calificacion3!) / 3;
+    this.submitted = true;
+  }
+
+  sonCalificacionesValidas(): boolean {
+    return (
+      this.calificacion1 !== null &&
+      this.calificacion2 !== null &&
+      this.calificacion3 !== null &&
+      this.esNumeroValido(this.calificacion1!) &&
+      this.esNumeroValido(this.calificacion2!) &&
+      this.esNumeroValido(this.calificacion3!)
+    );
+  }
+
+  esNumeroValido(numero: number | null): boolean {
+    return numero !== null && !isNaN(numero) && numero >= 0 && numero <= 100;
+  }
+
+  nuevasCalificaciones() {
+    this.calificacion1 = null;
+    this.calificacion2 = null;
+    this.calificacion3 = null;
+    this.promedio = null;
+    this.submitted = false;
+  }
+
 }
